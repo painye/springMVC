a90201989be80c57c37e2dc82e61c79d2a9b703f
painye <1850384745@qq.com>
2021-01-30 12:42:50 +0800

    关于几种控制器方法的返回类型的尝试

--- ch04-return/src/main/java/com/yp/controller/MyController.java
+++ ch04-return/src/main/java/com/yp/controller/MyController.java
@@ -1,14 +1,22 @@
 package com.yp.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yp.vo.Student;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -46,6 +54,61 @@ public class MyController {
         request.setAttribute("name", stu.getName());
         request.setAttribute("age", stu.getAge());
         request.setAttribute("student", stu);
+        //返回的是全路径，且一定要注意删除springMVC中的视图解析器
         return "/WEB-INF/view/show.jsp";
+    }
+
+    @RequestMapping(value = {"/returnVoid-ajax.do"})//string雷翔的返回值值返回视图也就是网页
+    public void returnVoid (String name, Integer age, HttpServletResponse response) throws IOException {
+       Student student = new Student();
+       student.setAge(age);
+       student.setName(name);
+
+        //将student转换为json格式
+        String json = "";
+        if(student != null){
+            ObjectMapper om = new ObjectMapper();
+            json = om.writeValueAsString(student);
+            System.out.println("===========转换后的student的json格式"+json);
+        }
+
+        //输出数据，响应json的请求
+        response.setContentType("application/json;charset = utf-8");
+        PrintWriter writer = response.getWriter();
+        writer.println(json);
+        writer.flush();
+        writer.close();
+    }
+
+    @ResponseBody  //实现将转换后的json数据上传至浏览器
+    @RequestMapping(value = {"/returnStudentJson.do"})//string雷翔的返回值值返回视图也就是网页
+    public Student returnStudentObject (String name, Integer age){
+        Student student = new Student();
+        student.setAge(30);
+        student.setName("李四");
+
+        //将student转换为json格式 注册驱动进行转为，实现HttoMessageConverter消息转换器下的七个实现类MapperJson2HttpMessage进行格式转换
+
+        //输出数据，响应json的请求，将转换后的json对象上传至浏览器
+        return student;
+    }
+
+
+    @ResponseBody  //实现将转换后的json数据上传至浏览器
+    @RequestMapping(value = {"/returnStudentList.do"})//string雷翔的返回值值返回视图也就是网页
+    public List<Student> returnStudentList (String name, Integer age){
+        List<Student> studentList = new ArrayList<>();
+        Student student1 = new Student();
+        Student student2 = new Student();
+        student2.setAge(30);
+        student2.setName("李四");
+        student1.setName("张三");
+        student1.setAge(20);
+        studentList.add(student1);
+        studentList.add(student2);
+        return studentList;
+        //将student转换为json格式 注册驱动进行转为，实现HttoMessageConverter消息转换器下的七个实现类MapperJson2HttpMessage进行格式转换
+
+        //输出数据，响应json的请求，将转换后的json对象上传至浏览器
     }
 }
--- ch04-return/target/ch04-return-1.0-SNAPSHOT/index.jsp
+++ ch04-return/target/ch04-return-1.0-SNAPSHOT/index.jsp
@@ -9,6 +9,33 @@
 <html>
 <head>
     <title>Title</title>
+    <!--外部导入这个库-->
+    <script src="js/jquery-3.3.1.min.js" type="text/javascript" ></script>
+    <script type="text/javascript">
+        $(function () {
+            $("button").click(function () {
+                //alert("click");
+               $.ajax({
+                   //提交给后台的地址
+                   url: "my/returnStudentList.do",
+                   //提交携带的信息
+                   data:{name:"张三", age:20},
+                   //提交的方式
+                   type:"post",
+                   //提交数据的类型
+                   dataType: "json",
+                   //回调函数， resp后台携带的数据
+                   success:function (resp) {
+                       //resp从服务器端返回的是一个json格式的字符串{"name": 张三, "age":20}
+                       //jquer会把字符串转换为json对象赋值给resp；
+                        alert(resp.name+"   "+resp.age)
+                   }
+
+               })
+            })
+
+        })
+    </script>
 </head>
 <body>
     <p>第一个springmvc项目</p>
@@ -29,5 +56,7 @@
         年龄<input type="text" name="age"><br/>
         <input type="submit" value="提交">
     </form>
+    <br/>
+    <button id="btn">发起ajax请求</button>
 </body>
 </html>
